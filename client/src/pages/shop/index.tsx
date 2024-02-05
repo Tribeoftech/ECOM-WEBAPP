d005d9f3b664bfb2520f90b0a6cc2e4b5a2a290d
Tribeoftech <gratefulgrowing333@gmail.com>
2024-02-05 12:12:35 -0600

    fix

---
+++ client/src/pages/shop/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
+import { Product } from "./product";
+
+import "./styles.css";
+import { useGetProducts } from "../../hooks/useGetProducts";
+
+export const ShopPage = () => {
+  const [cookies, _] = useCookies(["access_token"]);
+
+  const { products } = useGetProducts();
+
+  if (!cookies.access_token) {
+    return <Navigate to="/auth" />;
+  }
+
+  return (
+"products">
+        {products.map((product) => (
+          <Product product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
